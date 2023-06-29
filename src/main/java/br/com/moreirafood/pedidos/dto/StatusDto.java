8f1e56e7412fda9a1562a08eef74e83cc106f9dd
ttrmoreira <ttrmoreira@gmail.com>
2023-06-29 15:02:02 -0300

    Criação de um microseviço de pedidos

---
+++ src/main/java/br/com/moreirafood/pedidos/dto/StatusDto.java
@@ -0,0 +1,15 @@
+package br.com.moreirafood.pedidos.dto;
+
+import br.com.moreirafood.pedidos.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Status status;
+}
---
+++ src/main/java/br/com/moreirafood/pedidos/model/Status.java
@@ -0,0 +1,12 @@
+package br.com.moreirafood.pedidos.model;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
